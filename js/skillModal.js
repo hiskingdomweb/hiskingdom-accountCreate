be1d01daccee09e06e6283776d1de930778eedc3
Stephen Suffian <stephen.suffian@gmail.com>
2016-05-09 13:15:02 -0400

    updated skill modal for account create page

--- js/skillModal.js
+++ js/skillModal.js
@@ -1,43 +1,40 @@
 var inSkill = false;
 
 $('#assignSkill').click(function(){
+  $('#backButtonSkills').hide();
   inSkill = false;
   $('#skillBox').show();
   $('#skillChecklists').show();
   $('#skillLoadingText').hide();
-  $('#closeSkills').hide();
   $('#addSkills').show();
   $('#cancelSkills').show();
   $("#skillBox").html(skillBoxHTML);
   $('.skillSelect').hide();
   $('#skillModal').modal({
         keyboard: false,
-        backdrop: 'static'
+        backdrop: 'static',
     });
   
   $('.skillButton').click(function(){
         if(!inSkill){
-          $('.skillButton').not(this).each(function(){
+          $('.skillButton').each(function(){
             $(this).fadeOut('fast'); 
           })
-          $(this).animate({
-            height: '50px'
-          })
+
           loadSkillHTML($(this).text());
           inSkill = true;
-        }
-        else{
-          $('.skillButton').not(this).each(function(){
-            $(this).show('fast');  
-          })
-          $(this).animate({
-            height: '100px'
-          })
-          $('.skillSelect').fadeOut('fast');
-          inSkill = false;
+          $('#backButtonSkills').show();
         }
       })
-  
+})
+
+$('#backButtonSkills').click(function(){
+  $('.skillButton').each(function(){
+      $(this).show('fast');  
+    })
+    $('.skillSelect').fadeOut('fast');
+    $('#backButtonSkills').hide();
+    inSkill = false;
 })
 
 function loadSkills(skills){
@@ -48,14 +45,14 @@ function loadSkills(skills){
       })
     }
     var usertable;
-    $.getJSON("/accountCreate/listSkills",function(data){
-    //$.getJSON("json/rest/member/listSkills",function(data){
+    $.getJSON("/rest/accountCreate/listSkills",function(data){
         skillsHTML = "";
         skillChecklists = "";
         $.each(data.statements, function (key,category) {
-          categoryName = Object.keys(category)[0].replace(/\s/g, '');
+          categoryName = Object.keys(category)[0].replace(/\s/g, '').replace('/','');
           categorySkill = "";
-          skillChecklists += "<div id="+categoryName+"Skill class='skillSelect' >";
+          skillChecklists += "<div id="+categoryName+"Skill class='skillSelect'>"
+          +"<h3>"+Object.keys(category)[0]+"</h3>";
           $.each(category,function (key,skill) {
               $.each(skill,function(key,val){
                   if($.inArray(val.id,skillIds)>=0){
@@ -71,13 +68,12 @@ function loadSkills(skills){
           skillChecklists +="</div>";
         })
 
-        
+
         $("#skillChecklists").html(skillChecklists);
         $('.searchable-skill tr').show();
         $('.skillSelect').hide();
         $('#skillsHTML').html(skillsHTML);
 
-        
         for(var i=0;i<skillIds.length;i++){
            $("#"+skillIds[i]).prop('checked',true);
         }
@@ -112,33 +108,6 @@ $('#cancelSkills').click(function(){
   $('#skillList').html(skillString(cookSkills,genLaborSkills,handySkills));
 })
 */
-function skillString(cookSkills,genLaborSkills,handySkills){
-  cookString = "";
-  genLaborString = "";
-  handyString = "";
-  if(cookSkills.length + genLaborSkills.length + handySkills == 0){
-    return "None";
-  }
-  if(cookSkills.length==0){
-    cookString = "";
-  }
-  else{
-    cookString = "Cooking Skills: "+ cookSkills.toString()+"<br>";
-  }
-  if(genLaborSkills.length==0){
-    genLaborString = "";
-  }
-  else{
-    genLaborString = "General Labor Skills: "+ genLaborSkills.toString() +"<br>";
-  }
-  if(handySkills.length==0){
-    handyString = "";
-  }
-  else{
-    handyString = "Handy Skills: "+ handySkills.toString() +"<br>";
-  }
-  return cookString+genLaborString+handyString;
-}
 
 function loadSkillHTML(categoryName){
     $("#"+categoryName.replace(/\s/g, '')+"Skill").fadeIn('fast');
@@ -149,27 +118,27 @@ function resetSkillModal(){
   $('#skillList').html('None');
 }
 
-skillBoxHTML =  '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">General Labor</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Cook</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Handy</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Church Service</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Teach</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Social</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Kids</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Music</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Drama</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Grounds</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Clean</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Compassion</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Art</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Office</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Legal</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Engineering</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Crafts</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Business</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Language</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Mentor</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Counseling</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Mechanical</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Missions</button></div>'+
-                '<div class="col-md-4 skillButtonCol"><button type="button" class="btn btn-default skillButton">Speaking</button></div>';
+skillBoxHTML =  '<div class="col-md-4 skillButtonCol"><button type="button" value="ArtsandCrafts" class="btn btn-default skillButton">Arts and Crafts</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="biblical" class="btn btn-default skillButton">Biblical</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="churchservice" class="btn btn-default skillButton">Church Service</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="cleaning"  class="btn btn-default skillButton">Cleaning</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="compassion" class="btn btn-default skillButton">Compassion</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="cooking"  class="btn btn-default skillButton">Cooking</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="counseling" class="btn btn-default skillButton">Counseling</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="drama" class="btn btn-default skillButton">Drama</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="engineering" class="btn btn-default skillButton">Engineering</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="funding" class="btn btn-default skillButton">Funding</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="generallabor" class="btn btn-default skillButton">General Labor</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="grounds" class="btn btn-default skillButton">Grounds</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="homeimprovement" class="btn btn-default skillButton">Home Improvement</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="kids" class="btn btn-default skillButton">Kids</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="language" class="btn btn-default skillButton">Language</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="legal" class="btn btn-default skillButton">Legal</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="mechanical"  class="btn btn-default skillButton">Mechanical</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="medical"  class="btn btn-default skillButton">Medical</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="mentoring"  class="btn btn-default skillButton">Mentoring</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="missions"  class="btn btn-default skillButton">Missions</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="music"  class="btn btn-default skillButton">Music</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="office"  class="btn btn-default skillButton">Office-Business</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="social"  class="btn btn-default skillButton">Social</button></div>'+
+                '<div class="col-md-4 skillButtonCol"><button type="button" value="teaching"  class="btn btn-default skillButton">Teaching</button></div>';
